94fe4ce379329c4e58c9263dfe1d05082d5775cf
zvictorliu <zvictorliu@outlook.com>
2026-03-04 10:31:01 +0800

    feat(llm): 大模型流式输出接口

--- src/v_agent/session/llm.py
+++ src/v_agent/session/llm.py
@@ -1,6 +1,14 @@
 from ..provider.provider import ProviderOptions
 from langchain_qwq import ChatQwen
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import BaseMessage
+
+class StreamInput:
+    """流式输入格式"""
+    def __init__(self, messages : list[BaseMessage],
+                 tools, agent, system, sessionID):
+        self.messages = messages
+        self.sessionID = sessionID # 这些都是后话了
 
 class LLM:
     """大模型接入"""
@@ -20,3 +28,12 @@ class LLM:
             )
         else:
             raise ValueError(f"Unsupported provider: {options.provider}")
+        
+    def invoke(self, input):
+        '''需要把自己定义的消息格式转换成大模型需要的格式'''
+        pass
+
+    def stream(self, input): # 这里需定义自己的输入格式，其中包含 langchain 的消息格式
+        """流式处理用户问题"""
+        for chunk in self._client.stream(input.messages):
+            yield chunk
